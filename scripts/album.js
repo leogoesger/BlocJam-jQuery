fd7b4b3620683c2af4e17fee2822f6ef2e4e146d
LeoQ <leoq91@gmail.com>
2016-11-16 12:56:02 -0800

    before

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,21 @@ var albumPicasso = {
      ]
  };
 
+ var albumLeoo = {
+     title: 'The Pickle',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
